4a669b3f426915de1c2bc65a615f45e4da6a9f6c
Justin Shaw <jshaw22@gmail.com>
2017-07-30 22:48:02 -0700

    Build out messages component inital try

--- imports/ui/components/Message.jsx
+++ imports/ui/components/Message.jsx
@@ -3,23 +3,27 @@ import {browserHistory, Link} from 'react-router';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import moment from 'moment';
+import MessageThread from './MessageThread';
 
 class Messages extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			messages: []
+			contactsArray: [],
+			onePersonMessages: []
 		}
+		this.showThread = this.showThread.bind(this);
 		let data = {};
 		this.messageHandle = Meteor.subscribe('messageList');
+
 
 		//Seems like the Tracker takes the place of componentWillMount
 		Tracker.autorun(() => {
 		  const isReady = this.messageHandle.ready();
 		  if (isReady){
-		  	let messages = DBMessage.find({$or:[{'to._id':Meteor.userId()},{'fromuser':Meteor.userId()}]},{sort:{createdOn:-1}}).fetch();
+		  	let messages = DBMessage.find({$or:[{'to._id':Meteor.userId()},{'fromuser':Meteor.userId()}]},{sort:{createdOn:1}}).fetch();
 		  	this.transformMessages(messages)
-		  	this.setState({messages});
+		  	//this.setState({messages});
 		  }		
 		});
 	}
@@ -29,17 +33,34 @@ class Messages extends Component {
 	}
 
 	transformMessages(messages) {
-		user_id = Meteor.userId();
-		toUsers = messages.map((message) => message.to._id ).filter((id) => id != user_id);
-		fromUsers = messages.map((message) => message.fromuser ).filter((id) => id != user_id);
-		contacts = new Set(fromUsers.concat(toUsers));
-		hash = {}
-		contactMap = new Map();
-		for (var contact_id of contacts) {
-			contactMap.set(contact_id, this.messagesForContact(messages, contact_id) )
+		const user_id = Meteor.userId();
+		let toUsers = messages.map((message) => message.to._id ).filter((id) => id != user_id);
+		let fromUsers = messages.map((message) => message.fromuser ).filter((id) => id != user_id);
+		let uniqueContacts = [...new Set(fromUsers.concat(toUsers))]; // ES6 get unique values
+		let contactsArray = [];
+		for (let i = 0; i < uniqueContacts.length; i++) {
+			let messageInfo = {
+				contactKey: uniqueContacts[i],
+				contactName: this.contactUsername(messages, uniqueContacts[i]),
+				messagesWithContact: this.messagesForContact(messages, uniqueContacts[i])
+			}
+			contactsArray.push(messageInfo);
 		}
-		debugger
-		this.setState({contactMap: contactMap})
+		this.setState({contactsArray}, ()=>console.log("state contactsarray", this.state.contactsArray));
+	}
+
+	contactUsername (messages, contact_id) {
+		const messagesWithContact = this.messagesForContact(messages, contact_id);
+		let username;
+			// User was messaged first. Grab username of whoever messaged 
+			if (messagesWithContact[0].fromuser !== Meteor.userId())
+				username = messagesWithContact[0].fromUsername;
+			//User messaged someone first. grab username of whoever messaged
+			else if (messagesWithContact[0].fromuser === Meteor.userId())
+				username = messagesWithContact[0].to.username; 
+			else 
+				console.error("messages structure was changed");
+		return username; 
 	}
 
 	messagesForContact(messages, contact_id) {
@@ -49,23 +70,44 @@ class Messages extends Component {
 		)
 	}
 
+	showThread (evt) {
+		const contactKey = evt.currentTarget.dataset.id;
+		let array = this.state.contactsArray.find(item => {
+			return item.contactKey = contactKey;
+		});
+		this.setState({onePersonMessages: array});
+	}
 
-	renderConversations() {
-		let conversations = []
-		for (var conversation of this.state.contactMap) {
-			converstations.push(<Conversation {conversation} />)
+	renderMessageThread() {
+		if (this.state.onePersonMessages.length > 0){
+			return this.state.onePersonMessages.messagesWithContact.map(message => {
+				console.log("message message", message.message)
+				return (
+					<div>{message.message}</div>
+				)
+			});
 		}
-
-		return conversations
+	}
+	
+	renderMessageThreads() {
+		return this.state.contactsArray.map((thread, index) => {
+			return (
+				<MessageThread threadClicked={this.showThread} thread={this.state.contactsArray[index]} />
+			)
+		});
 	}
 
 	render() {
-		if (this.state.contactMap == null) {
-			return null;
+		const contactMap = this.state.contactMap;
+		if (contactMap && contactMap.size === 0) {
+			return (<div>No messages to display</div>);
 		}
-
-		return {this.renderConversations()}
-
+		return (
+			<div>
+				<ul>{this.renderMessageThreads()}</ul>
+				{this.renderMessageThread()}
+			</div>
+		)
 	}
 
 
