65866cd33ff16632b3c79b79519069015a721123
wachaw <wachaw@ebay.com>
2018-03-04 23:40:15 -0800

    direction

--- imports/ui/components/App.jsx
+++ imports/ui/components/App.jsx
@@ -100,7 +100,7 @@ class App extends Component {
 		console.log("User logged in?", loggedIn);
 		return (
 			<div>
-				<nav className="navbar navbar-expand-md navbar-light bg-light">
+				<nav className="navbar navbar-expand-md navbar-light bg-light flex-row-reverse flex-md-row">
 				  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				    <span className="navbar-toggler-icon"></span>
 				  </button>
